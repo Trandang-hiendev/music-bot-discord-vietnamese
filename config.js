1fc480111fc96afd80a0f62ef78c38959000db9a
Danghiendevbot001 <135580941+Trandang-hiendev@users.noreply.github.com>
2024-03-25 13:46:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  Admins: ["UserID", "UserID"], //Admins of the bot
-  ExpressServer: true,//If you wanted to make the website run or not
+  Admins: ["1114889680484315206", "UserID"], //Admins of the bot
+  ExpressServer: false,//If you wanted to make the website run or not
   DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
-  SupportServer: "https://discord.gg/sbySMS7m3v", //Support Server Link
+  SupportServer: "https://discord.gg/6TZDXQNHFC", //Support Server Link
   Token: process.env.Token || "YourBotToken", //Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "YourBotClientID", //Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "YourBotClientSecret", //Discord Client Secret
+  ClientID: process.env.Discord_ClientID || "1221699838186360884", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "kkNMAjWkxICh7HGEi1XS_FJE93Efozn4", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
   "24/7": false, //If you want the bot to be stay in the vc 24/7
@@ -36,7 +36,7 @@ module.exports = {
 
   //Please go to https://developer.spotify.com/dashboard/
   Spotify: {
-    ClientID: process.env.Spotify_ClientID || "YourSpotifyID", //Spotify Client ID
-    ClientSecret: process.env.Spotify_ClientSecret || "YourSpotifySecret", //Spotify Client Secret
+    ClientID: process.env.Spotify_ClientID || "d4ba8f36fe9c433a8d4daa9468479828", //Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "1b587c9a2ee24e6f91663a4215f2180e", //Spotify Client Secret
   },
 };
